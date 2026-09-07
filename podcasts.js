4f2dfeecd8c939d3a7b42d5086479a65e2d5eeda
robot616 <moomani@protonmail.com>
2026-09-07 06:12:54 +0300

    Change variable declaration to window property

--- podcasts.js
+++ podcasts.js
@@ -1,4 +1,4 @@
-const memoryJourneyEpisodes = [
+window.memoryJourneyEpisodes = [
     { name: 'كيف أنقذ الماسونيون كاسترو من الإعدام؟', url: "https://cdnv.russiatoday.com/media/audio/2026.09/6a9d6a33423604207a52d242.mp3" },
     { name: 'هل سنسلم شؤوننا إلى الآلات طوعا أو كرها؟', url: "https://cdnv.russiatoday.com/media/audio/2026.08/6a9419d64c59b716871dd107.mp3" },
     { name: 'التجسس الرقمي والسيادة الرقمية', url: "https://cdnv.russiatoday.com/media/audio/2026.08/6a8a200342360465173b5b24.mp3" },
